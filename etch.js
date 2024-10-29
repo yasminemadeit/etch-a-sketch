f1e1d58c20dbb6abee217f6b3607c62e60ef21f7
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-29 19:56:13 -0400

    Create grid

---
+++ etch.js
@@ -0,0 +1,33 @@
+const mainContainer = document.querySelector("#main-container");
+let gridHeight= 16;
+let gridWidth =16;
+const containerWidth = 960;
+const containerHeight = 960;
+
+mainContainer.style.width = containerWidth + "px";
+mainContainer.style.height = containerHeight +  "px";
+mainContainer.style.display = "flex";
+mainContainer.style.flexWrap = "wrap";
+
+
+let relativeWidth= containerWidth/gridWidth + "px";
+let relativeHeight =  (containerHeight/gridHeight) + "px";
+
+
+for (let i = 0; i < gridHeight; i++){
+    for (let j=0; j<gridWidth; j++){
+    let gridDiv = document.createElement("div");
+    gridDiv.id = "div-" + i + "-" + j;
+    gridDiv.style.background = "blue";
+    gridDiv.style.border = "solid,black,1px";
+    gridDiv.style.height = relativeHeight;
+    gridDiv.style.width = relativeWidth;
+    gridDiv.style.boxSizing = "border-box";
+
+
+    mainContainer.appendChild(gridDiv);
+    }
+
+
+}
+
