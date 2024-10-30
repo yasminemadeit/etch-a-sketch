e90b853f0ec06a6cde07db66c65e0371cd153348
Yasmine <20632722+yasminemadeit@users.noreply.github.com>
2024-10-29 20:44:45 -0400

    New grid generated on button click

--- etch.js
+++ etch.js
@@ -1,37 +1,57 @@
 const mainContainer = document.querySelector("#main-container");
-let gridHeight= 16;
-let gridWidth =16;
 const containerWidth = 960;
 const containerHeight = 960;
+const btn = document.querySelector("#btn");
 
-mainContainer.style.width = containerWidth + "px";
-mainContainer.style.height = containerHeight +  "px";
-mainContainer.style.display = "flex";
-mainContainer.style.flexWrap = "wrap";
+let userInput;
 
-
-let relativeWidth= containerWidth/gridWidth + "px";
-let relativeHeight =  (containerHeight/gridHeight) + "px";
-
-//Create Grid
-for (let i = 0; i < gridHeight; i++){
-    for (let j=0; j<gridWidth; j++){
-    let gridDiv = document.createElement("div");
-    gridDiv.id = "div-" + i + "-" + j;
-    gridDiv.style.background = "grey";
-    gridDiv.style.border = "solid,black,1px";
-    gridDiv.style.height = relativeHeight;
-    gridDiv.style.width = relativeWidth;
-    gridDiv.style.boxSizing = "border-box";
-
-    gridDiv.addEventListener("mouseover", function(){
-        this.style.background = "black";
-    });
+btn.addEventListener("click", function(){
+    do {
+        userInput = prompt("Please enter an integer between 1 and 100:)");
+        gridSize = parseInt(userInput);
+    } while (isNaN(userInput)|| userInput>100 || userInput<1);
+    mainContainer.innerHTML= "";
+    newGrid(gridSize);
 
 
-    mainContainer.appendChild(gridDiv);
+
+});
+
+
+function newGrid(gridSize){
+
+
+   
+    mainContainer.style.width = containerWidth + "px";
+    mainContainer.style.height = containerHeight +  "px";
+    mainContainer.style.display = "flex";
+    mainContainer.style.flexWrap = "wrap";
+    let relativeWidth= containerWidth/gridSize + "px";
+    let relativeHeight =  (containerHeight/gridSize) + "px";
+
+    //Create Grid
+    for (let i = 0; i < gridSize; i++){
+        for (let j=0; j<gridSize; j++){
+        let gridDiv = document.createElement("div");
+        gridDiv.id = "div-" + i + "-" + j;
+        gridDiv.style.background = "grey";
+        gridDiv.style.border = "solid,black,1px";
+        gridDiv.style.height = relativeHeight;
+        gridDiv.style.width = relativeWidth;
+        gridDiv.style.boxSizing = "border-box";
+
+        gridDiv.addEventListener("mouseover", function(){
+            this.style.background = "black";
+        });
+
+
+        mainContainer.appendChild(gridDiv);
+        }
     }
+
+
+
+    
+
 }
-
-
 
